a8d980f4121b3360e5518bac304674a5cfc3dd7b
yprsty <yprsty01@gmail.com>
2020-06-25 17:06:04 +0700

    Updates

---
+++ precache-manifest.444ac6499be1e5a4fa45fca4d01e951a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb7ecb3e40a85d87440894d41a7e13f",
+    "url": "/react-footbal-app/index.html"
+  },
+  {
+    "revision": "58b7459597e1d71f1ca7",
+    "url": "/react-footbal-app/static/css/main.0ea4a77d.chunk.css"
+  },
+  {
+    "revision": "639fc1d6a5374e183dac",
+    "url": "/react-footbal-app/static/js/2.4ffd5ee4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-footbal-app/static/js/2.4ffd5ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b7459597e1d71f1ca7",
+    "url": "/react-footbal-app/static/js/main.72806513.chunk.js"
+  },
+  {
+    "revision": "b4f9fca422a4ac249b2c",
+    "url": "/react-footbal-app/static/js/runtime-main.ce15642f.js"
+  }
+]);
